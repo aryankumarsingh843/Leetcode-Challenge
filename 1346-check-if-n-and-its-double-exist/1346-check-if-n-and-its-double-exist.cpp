dbea3748d69ed2563afa2da9368c85050ddcc92e
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-06-25 02:24:31 +0530

    Time: 0 ms (100%), Space: 13.5 MB (85.63%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -3,18 +3,6 @@ public:
     bool checkIfExist(vector<int>& arr) {
         int n = arr.size();
 
-       /* sort (arr.begin(), arr.end());
-        int i=0;
-        int j=n-1;
-        while (i<j){
-            if (arr[j] == 2*arr[i]) return true;
-            
-            else if (arr[j] > 2*arr[i]) i++;
-
-            else j--;
-        }
-        return false; */
-
         for (int i=0; i<n; i++){
             for (int j=0; j<n; j++){
                 if (i != j && arr[i] == 2*arr[j]){
@@ -22,6 +10,6 @@ public:
             }
          }
         }
-        return false;
+        return false; 
     }
 };
