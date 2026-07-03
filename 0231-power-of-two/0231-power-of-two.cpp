b094141c79179fd91934c28911b1a4980855a240
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-07-03 22:57:35 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.65%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-     if (n<=0) return false;
 
-     return (n&(n-1)) == 0;
+    // Bit - Manipulation method
+    /* if (n<=0) return false;
+
+     return (n&(n-1)) == 0; 
+    }*/
+
+    // Recursion method
+
+    if (n<=0) return false;
+    if (n==1) return true;
+
+    return n%2==0 && isPowerOfTwo(n/2);
     }
 };
