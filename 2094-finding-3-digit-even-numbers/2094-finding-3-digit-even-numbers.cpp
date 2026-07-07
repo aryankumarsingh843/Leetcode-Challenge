07cbc2264306d54b02a17abd02dbfa577f5f05be
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-07-07 23:52:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (96.74%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
-      int n = digits.size();
+    /*  int n = digits.size();
       unordered_set<int> st;
       for (int i=0; i<n; i++){
         for (int j=0; j<n; j++){
@@ -15,6 +15,32 @@ public:
       }
       vector <int> result(begin(st), end(st));
       sort (begin(result), end(result));
-      return result;
+      return result; */
+
+      vector <int> v;
+      vector <int> mp(10, 0);
+
+      for (int i=0; i<digits.size(); i++){
+        mp[digits[i]]++;
+      }
+
+      for (int i=1; i<=9; i++){
+        if (mp[i] == 0) continue;
+        mp[i]--;
+        for (int j=0; j<=9; j++){
+        if (mp[j] == 0) continue;
+        mp[j]--;
+            for (int k=0; k<=8; k+=2){
+            if (mp[k] == 0) continue;
+            mp[k]--;  
+            int num = i*100 +j*10 +k;
+            v.push_back(num);
+            mp[k]++;
+            }
+            mp[j]++;
+        }
+        mp[i]++;
+      }
+      return v;
     }
 };
