cede3f0e4d2856898daeb6bc075d12445a72825b
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-09-03 00:53:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (71.24%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string greatestLetter(string s) {
+        bool lower[26] = {false};
+        bool upper[26] = {false};
+
+        for (int i=0; i<s.size(); i++){
+            if (s[i] >= 'A' && s[i] <= 'Z'){
+                upper[s[i]-'A'] = true;
+            }
+            else {
+                lower[s[i]-'a'] = true;
+            }
+        }
+
+        for (int i=25; i>=0; i--){
+            if (lower[i] && upper[i]){
+                return string(1, 'A'+i);
+            }
+        }
+        return "";
+    }
+};
