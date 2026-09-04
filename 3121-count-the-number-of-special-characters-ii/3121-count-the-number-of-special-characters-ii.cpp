ab55b7924e425b859531604fedc32df3624fcfd2
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-09-05 01:26:33 +0530

    Time: 52 ms (98.69%), Space: 24.6 MB (37.6%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        int lower[26];
-        int upper[26];
-        
-        for (int i=0; i<26; i++){
-            lower[i] = -1;
-            upper[i] = -1;
-        }
+       int lower[26];
+       int upper[26];
 
-        for (int i=0; i<word.size(); i++){
-            if (word[i] >= 'a' && word[i] <= 'z'){
-                lower[word[i]-'a'] = i;
-            }
-            else {
-                if (upper[word[i] - 'A'] == -1){
+       for (int i=0; i<26; i++){
+        lower[i] = -1;
+        upper[i] = -1;
+       }
+
+       for (int i=0; i<word.size(); i++){
+        if (word[i] >= 'a' && word[i] <= 'z'){
+            lower[word[i]-'a'] = i;
+        }
+        else {
+            if (upper[word[i]-'A'] == -1){
                 upper[word[i]-'A'] = i;
             }
-            }
         }
-        int ans = 0;
+       }
 
-        for (int i=0; i<26; i++){
-           if (lower[i] != -1 && upper[i] != -1 && lower[i] < upper[i]) ans++;
-        }
+       int ans = 0;
 
-        return ans;
+       for (int i=0; i<26; i++){
+        if (lower[i] != -1 && upper[i] != -1 && lower[i] < upper[i]) ans++;
+       }
+
+       return ans;
     }
 };
