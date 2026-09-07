158ea687dd102e53efd04372fa6d5cbe0f34f1b9
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-09-08 01:06:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (61.56%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.cpp
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int countPoints(string rings) {
-        int arr[10][3] = {};
+     int arr[10][3]  = {};
 
-        for (int i=0; i<rings.size(); i+=2){
-            char colour = rings[i];
-            int rod = rings[i+1] - '0';
+     for (int i=0; i<rings.size(); i+=2){
+        int colour = rings[i];
+        int rod = rings[i+1]-'0';
 
-            if (colour == 'R'){
-                arr[rod][0] = 1;
-            }
-
-            else if (colour == 'G'){
-                arr[rod][1] = 1;
-            }
-
-            else {
-                arr[rod][2] = 1;
-            }
+        if (colour == 'R'){
+            arr[rod][0] = 1;
         }
 
-        int ans = 0;
-
-        for (int i=0; i<10; i++){
-            if (arr[i][0] && arr[i][1] && arr[i][2]) ans++;
+        else if (colour == 'G'){
+            arr[rod][1] = 1;
         }
 
-        return ans;
+        else arr[rod][2] = 1;
+     }
+
+     int ans = 0;
+
+     for (int i=0; i<10; i++){
+        if (arr[i][0] && arr[i][1] && arr[i][2]) ans++;
+     }
+
+     return ans;
     }
 };
