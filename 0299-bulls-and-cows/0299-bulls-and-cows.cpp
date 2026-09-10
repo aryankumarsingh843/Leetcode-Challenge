371bfbf3294601fa5fa189bb58d93208349fbf1a
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-09-11 00:52:17 +0530

    Time: 0 ms (100%), Space: 8.9 MB (51.57%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bull = 0;
+        int freqsecret[10] = {0};
+        int freqguess[10] = {0};
+
+        for (int i=0; i<secret.size(); i++){
+            if (secret[i] == guess[i]) bull++;
+            else {
+                freqsecret[secret[i]-'0']++;
+                freqguess[guess[i]-'0']++;
+            }
+        }
+
+        int cow=0;
+
+        for (int i=0; i<10; i++){
+            cow += min(freqsecret[i], freqguess[i]);
+        }
+
+        return to_string(bull)+'A' + to_string(cow)+'B';
+    }
+};
