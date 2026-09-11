9b3919e6ae44d38e84d8cb1ae704853396ac0d16
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-09-12 01:56:09 +0530

    Time: 0 ms (100%), Space: 9 MB (99.85%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
-        if (s.size() != goal.size()) return false;
+       if (s.size() != goal.size()) return false;
 
-        if (s == goal){
-            int freq[26] = {0};
-
-            for (int i=0; i<s.size(); i++){
-                freq[s[i]-'a']++;
-
-                if (freq[s[i] - 'a'] >= 2) return true;
-            }
-            return false;
-        }
-
-        int first = -1;
-        int second = -1;
-        int count = 0;
+       if (s==goal){
+        int freq[16] = {0};
 
         for (int i=0; i<s.size(); i++){
-            if (s[i] != goal[i]) {
-                count++;
+            freq[s[i]-'a']++;
+
+            if (freq[s[i]-'a'] >= 2) return true;
+        }
+        return false;
+       }
+
+       int count = 0;
+       int first = -1;
+       int second = -1;
+
+       for (int i=0; i<s.size(); i++){
+        if (s[i] != goal[i]){
+            count++;
             if (first == -1) first = i;
             else second = i;
         }
-        }
-        if (count != 2) return false;
+       }
 
-        return s[first] == goal[second] && s[second] == goal[first];
+       if (count != 2) return false;
+       return s[first]==goal[second] && s[second]==goal[first];
     }
 };
