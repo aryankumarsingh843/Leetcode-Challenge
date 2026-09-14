8399111fcea82a81c90fc310db95235d85770907
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-09-15 00:55:58 +0530

    Time: 363 ms (35.11%), Space: 93.4 MB (23.56%) - LeetHub

--- 3265-count-almost-equal-pairs-i/3265-count-almost-equal-pairs-i.cpp
+++ 3265-count-almost-equal-pairs-i/3265-count-almost-equal-pairs-i.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     bool almostequal(int a, int b){
-       string s = to_string(a);
-       string t = to_string(b);
+        string s = to_string(a);
+        string t = to_string(b);
 
-       while (s.size() < t.size()) 
-       s = "0" + s;
+        while (s.size() < t.size())
+        s = "0"+s;
 
-       while (t.size() < s.size())
-       t = "0" + t;
+        while (t.size() < s.size())
+        t = "0"+t;
 
-       if (s==t) return true;
+        if (s==t) return true;
 
-       vector <int> v;
+        vector <int> v;
 
-       for (int i=0; i<s.size(); i++){
-        if (s[i] != t[i]) v.push_back(i);
-       }
+        for (int i=0; i<s.size(); i++){
+            if (s[i] != t[i]){
+                v.push_back(i);
+            }
+        }
 
+        if (v.size() != 2) return false;
 
-       if (v.size() != 2) return false;
+        swap(s[v[0]], s[v[1]]);
 
-       swap(s[v[0]], s[v[1]]);
-
-       return s==t;
-}
+        return s==t;
+    }
     int countPairs(vector<int>& nums) {
-        int ans = 0;
         int n = nums.size();
+        int ans = 0;
 
         for (int i=0; i<n; i++){
             for (int j=i+1; j<n; j++){
                 if (almostequal(nums[i], nums[j])) ans++;
             }
         }
+
         return ans;
     }
 };
