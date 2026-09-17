da2d36ea18125cb22f96099d4027584884789eb3
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-09-17 17:48:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (92.15%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        
-       stringstream ss(s);
+        stringstream ss(s);
+        vector <string> word;
+        string ans;
 
-       vector <string> word;
-
-       string ans;
-
-       while (ss >> ans){
-        word.push_back(ans);
-       }
-       
-       if (pattern.size() != word.size()) return false;
-       
-       for (int i=0; i<pattern.size(); i++){
-        for (int j=0; j<pattern.size(); j++){
-            if (pattern[i] == pattern[j] && word[i] != word[j])  return false;
-
-            if (pattern[i] != pattern[j] && word[i] == word[j])  return false;
+        while (ss >> ans){
+            word.push_back(ans);
         }
-       }
 
-       return true;
+        if (pattern.size() != word.size()) return false;
+
+        for (int i=0; i<pattern.size(); i++){
+            for (int j=0; j<pattern.size(); j++){
+                if (pattern[i] == pattern[j] && word[i] != word[j]) return false;
+
+                if (pattern[i] != pattern[j] && word[i] == word[j]) return false;
+            }
+        }
+
+        return true;
     }
 };
