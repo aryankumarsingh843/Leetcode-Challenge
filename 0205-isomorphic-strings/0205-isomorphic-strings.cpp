d3563f0f3122e9a97eafda8ad84c38e86c1df45f
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-09-17 17:27:34 +0530

    Time: 6 ms (8.41%), Space: 10.6 MB (7.47%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,9 +3,28 @@ public:
     bool isIsomorphic(string s, string t) {
         if (s.size() != t.size()) return false;
 
+        vector <int> p1;
+        vector <int> p2;
+
         for (int i=0; i<s.size(); i++){
-            if (s.find(s[i]) != t.find(t[i])) return false;
+            for (int j=0; j<=i; j++){
+                if (s[i] == s[j]){
+                    p1.push_back(j);
+                    break;
+                }
+            }
         }
+
+        for (int i=0; i<t.size(); i++){
+            for (int j=0; j<=i; j++){
+                if (t[i] == t[j]){
+                    p2.push_back(j);
+                    break;
+                }
+            }
+        }
+
+        if (p1 != p2) return false;
         return true;
     }
 };
