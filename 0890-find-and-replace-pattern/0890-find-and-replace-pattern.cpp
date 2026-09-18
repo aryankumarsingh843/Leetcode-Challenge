1bdbc9bfe95c3928862b0aecaf28e67ffe0e46be
Aryan Kumar Singh <aryankumarbnk90@gmail.com>
2026-09-18 18:28:23 +0530

    Time: 0 ms (100%), Space: 12.1 MB (7.27%) - LeetHub

---
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool match(string word, string pattern){
+        int mp1[256] = {0};
+        int mp2[256] = {0};
+
+        for (int i=0; i<word.size(); i++){
+            if (mp1[word[i]] != 0 && mp1[word[i]] != pattern[i]) return false;
+
+            if (mp2[pattern[i]] != 0 && mp2[pattern[i]] != word[i]) return false;
+
+            mp1[word[i]] = pattern[i];
+            mp2[pattern[i]] = word[i];
+        }
+        return true;
+    }
+    
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+
+        vector <string> ans;
+
+        for (int i=0; i<words.size(); i++){
+            if (match(words[i], pattern)) ans.push_back(words[i]);
+        }
+
+        return ans;
+    }
+};
